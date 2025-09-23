d91eb92998d7543fd3da4c2ebfcf5b667bacf1bb
Shreya Joshi <152019323+SSJoshi1808@users.noreply.github.com>
2025-09-23 22:33:14 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -128,9 +128,9 @@ app.get("/", (req, res) => {
     res.send("Welcome to SuvarnaRup");
 });
 
-// ✅ Start the Server
-//app.listen(PORT, () => {
+ ✅ Start the Server
+app.listen(PORT, () => {
     //console.log(`Server is running on port ${PORT}`);
-//});
+});
 
-module.exports = app;
+//module.exports = app;
