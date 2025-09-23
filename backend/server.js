a296e7e088677ea6960306ca01846a5431928b88
Shreya Joshi <152019323+SSJoshi1808@users.noreply.github.com>
2025-09-23 22:27:11 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -129,6 +129,8 @@ app.get("/", (req, res) => {
 });
 
 // ✅ Start the Server
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+//app.listen(PORT, () => {
+    //console.log(`Server is running on port ${PORT}`);
+//});
+
+module.exports = app;
